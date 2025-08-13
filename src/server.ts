2ee0f97f6084e4a36691cd53f18f11774423d7ed
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2025-08-14 03:59:40 +0900

    feat: prepare for production

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@
 //Design pattern: Middleware, Decorator
 //import moment  from "moment";     //module js da bolsa require orniga import ni ishlatyapmiz  // const moment = require('moment'); common js da shunde ishlatganmiz
 import dotenv from "dotenv";
-dotenv.config(); // manashuyerda config metodini qolladik va shu yerda dotenv environmental variablelarni integratsiyasini amalga oshirdik
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+}); // manashuyerda config metodini qolladik va shu yerda dotenv environmental variablelarni integratsiyasini amalga oshirdik
 import mongoose from "mongoose";
 import server from "./app";
 mongoose
